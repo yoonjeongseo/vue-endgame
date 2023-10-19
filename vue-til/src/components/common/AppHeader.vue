ead469a6442367788c66043842e3411b638528be
SEOYOONJEONG-ez <112161928+yoonjeongseo@users.noreply.github.com>
2023-10-19 17:26:43 +0900

    로그아웃 기능에러 수정

--- vue-til/src/components/common/AppHeader.vue
+++ vue-til/src/components/common/AppHeader.vue
@@ -23,6 +23,8 @@
 </template>
 
 <script>
+import { deleteCookie } from '@/utils/cookies';
+
 export default {
   computed: {
     isUserLogin() {
@@ -35,6 +37,9 @@ export default {
   methods: {
     logoutUser() {
       this.$store.commit('clearUsername');
+      this.$store.commit('clearToken');
+      deleteCookie('til_auth');
+      deleteCookie('til_user');
       this.$router.push('/login');
     },
   },
