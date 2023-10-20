aa7195691dfe0d2864431b6d7fb995c1623de647
SEOYOONJEONG-ez <112161928+yoonjeongseo@users.noreply.github.com>
2023-10-20 11:32:26 +0900

    jest 코드 작성

---
+++ vue-til/src/components/LoginForm.spec.js
@@ -0,0 +1,13 @@
+import { shallowMount } from '@vue/test-utils';
+// import Vue from 'vue';
+import LoginForm from './LoginForm.vue';
+
+describe('LoginForm.vue', () => {
+  test('컴포넌트가 마운트되면 username이 존재하고 초기 값으로 설정되어 있어야 한다', () => {
+    // const instance = new Vue(LoginForm).$mount();
+    const wrapper = shallowMount(LoginForm);
+    // expect(wrapper.vm.username).toBe('');
+    const idInput = wrapper.find('#username');
+    console.log(idInput.html());
+  });
+});
