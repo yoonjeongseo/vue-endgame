bd1a4f14ad7ca5b3eb190d9a038650ee643673c3
SEOYOONJEONG-ez <112161928+yoonjeongseo@users.noreply.github.com>
2023-10-20 15:19:10 +0900

    jest 검사 완료

--- vue-til/src/components/LoginForm.spec.js
+++ vue-til/src/components/LoginForm.spec.js
@@ -3,7 +3,7 @@ import { shallowMount } from '@vue/test-utils';
 import LoginForm from './LoginForm.vue';
 
 describe('LoginForm.vue', () => {
-  test('id는 이메일 형식이어야 한다', () => {
+  test('id는 이메일 형식이 아니면 경고가 나온다', () => {
     // const instance = new Vue(LoginForm).$mount();
     const wrapper = shallowMount(LoginForm, {
       data() {
@@ -16,5 +16,16 @@ describe('LoginForm.vue', () => {
     expect(warningTxt.exists()).toBeTruthy();
   });
 
-  test('아이디와 패스워드가 입력되지 않으면 로그인 버튼이 비활성화 된다', () => {});
+  test('아이디와 패스워드가 입력되지 않으면 로그인 버튼이 비활성화 된다', () => {
+    const wrapper = shallowMount(LoginForm, {
+      data() {
+        return {
+          username: '',
+          password: '',
+        };
+      },
+    });
+    const btn = wrapper.find('.btn');
+    expect(btn.element.disabled).toBeTruthy();
+  });
 });
