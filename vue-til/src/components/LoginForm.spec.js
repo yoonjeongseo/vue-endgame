6e3949083d30fce5fb1bb112b082f0c58861ec23
SEOYOONJEONG-ez <112161928+yoonjeongseo@users.noreply.github.com>
2023-10-20 11:46:32 +0900

    로그인폼 테스트 코드 작성

--- vue-til/src/components/LoginForm.spec.js
+++ vue-til/src/components/LoginForm.spec.js
@@ -5,9 +5,16 @@ import LoginForm from './LoginForm.vue';
 describe('LoginForm.vue', () => {
   test('컴포넌트가 마운트되면 username이 존재하고 초기 값으로 설정되어 있어야 한다', () => {
     // const instance = new Vue(LoginForm).$mount();
-    const wrapper = shallowMount(LoginForm);
+    const wrapper = shallowMount(LoginForm, {
+      data() {
+        return {
+          username: 'test@abc.com',
+        };
+      },
+    });
     // expect(wrapper.vm.username).toBe('');
     const idInput = wrapper.find('#username');
-    console.log(idInput.html());
+    console.log('input', idInput.element.value);
+    console.log(wrapper.vm.isUsernameValid);
   });
 });
